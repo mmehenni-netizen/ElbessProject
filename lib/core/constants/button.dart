dfdea1de6b1e422136f6d6ee1f44a8360266d766
Sifou-2 <ms.madani@esi-sba.dz>
2026-02-21 17:02:11 +0100

    Update login UI and social auth assets

--- lib/core/constants/button.dart
+++ lib/core/constants/button.dart
@@ -1,6 +1,7 @@
 import 'package:elbess/core/constants/colors.dart';
 import 'package:elbess/core/utils/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.text, this.OnTap});
   final String text;
@@ -30,5 +31,76 @@ class CustomButton extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class OptionsButton extends StatelessWidget {
+  const OptionsButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  
+                  Container(
+                    
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Color(0xFFD9D9D9),width: 1),
+                ),
+                
+                child: Center(
+                  child: Image.asset(
+                    'assets/Images/google.png',
+                    width: 20,
+                    height: 20,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Gap(13),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Color(0xFFD9D9D9),width: 1),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/Images/facebook.png',
+                    width: 20,
+                    height: 20,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Gap(13),
+              
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Color(0xFFD9D9D9),width: 1),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/Images/apple.png',
+                    width: 20,
+                    height: 20,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              )
+                ],
+
+              );
   }
 }
